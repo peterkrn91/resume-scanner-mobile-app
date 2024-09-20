c708a2b455fe918a8791350c3efc5a9dfdfe3ee1
peter <peter_krn@hotmail.com>
2024-09-20 23:28:02 +0700

    initial commit

---
+++ app/src/main/java/com/example/resumescannerapp/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.resumescannerapp
+
+import android.content.Intent
+import com.example.resumescannerapp.R
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val getStartedButton : Button = findViewById(R.id.getstarted_button)
+
+        getStartedButton.setOnClickListener(this)
+
+
+    }
+
+    override fun onClick(v:View?) {
+        if (v != null) {
+            when(v.id) {
+                R.id.getstarted_button -> {
+                    startActivity(Intent(this@MainActivity, HomeActivity::class.java))
+                }
+            }
+        }
+    }
+}
